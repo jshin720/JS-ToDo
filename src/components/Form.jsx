de112578c0d20ccb869ff6c76f2c7cb623cf5bf8
JShin720 <jeffshin720@gmail.com>
2024-07-30 13:49:10 -0400

    creating add task input

---
+++ src/components/Form.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class form extends Component {
+  render() {
+    return (
+      <div>
+        <input type="text" name="" id="" />
+        <button type="button">add task</button>
+      </div>
+    );
+  }
+}
