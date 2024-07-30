c3624559e132fc1d3bdde3ff68a308ff0ad8380e
JShin720 <jeffshin720@gmail.com>
2024-07-30 13:55:26 -0400

    creating add task input

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class form extends Component {
+  state = {
+    inputValue: "",
+    toDos: []
+  };
+
+  submitHandler = = (e) => {
+    this.setState({
+        toDos: [this.state.inputValue]
+    });
+  };
+
   render() {
     return (
       <div>
-        <input type="text" name="" id="" />
+        <input
+          type="text"
+          placeholder="Enter Task"
+          value={this.state.inputValue}
+          id="newTask"
+        />
         <button type="button">add task</button>
+        <ol>
+            {this.state.toDos.map((todo) => (
+                <li>{todo}</li>
+            )
+            )}
+        </ol>
       </div>
     );
   }
