cbbae3c07161a178a69788ac99175e7867a39315
JShin720 <jeffshin720@gmail.com>
2024-08-05 17:12:38 -0400

    creating change handlers for both inputs

--- src/components/monthly/Monthly.jsx
+++ src/components/monthly/Monthly.jsx
@@ -4,7 +4,8 @@ export default function monthly() {
   const [month, setMonth] = useState(new Date().getMonth() + 1);
   const [year, setYear] = useState(new Date().getFullYear());
     
-
+  const [days, setDays] = useState([]);
+  const [day, setDay] = useState(1);
 
   return (
     <div>
