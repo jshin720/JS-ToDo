857ed5e3cda1177f2d69f16ad2fc71f70bd4a324
JShin720 <jeffshin720@gmail.com>
2024-08-06 15:20:05 -0400

    creating change handlers for both inputs

--- src/components/weekly/Weekly.jsx
+++ src/components/weekly/Weekly.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Weekly() {
+  const [week, setWeek] = useState([
+    {day: 'Monday', tasks: []},
+    {day: 'Tuesday', tasks: []},
+    {day: 'Wednesday', tasks: []},
+    {day: 'Thursday', tasks: []},
+    {day: 'Friday', tasks: []},
+    {day: 'Saturday', tasks: []},
+    {day: 'Sunday', tasks: []}
+    ]);
+
+
   return (
     <div>
       
