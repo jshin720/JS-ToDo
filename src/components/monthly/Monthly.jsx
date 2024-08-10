43e95b97b17b15662399b7b0436e266a35c35917
JShin720 <jeffshin720@gmail.com>
2024-08-09 23:38:44 -0400

    creating change handlers for both inputs

--- src/components/monthly/Monthly.jsx
+++ src/components/monthly/Monthly.jsx
@@ -2,8 +2,8 @@ import React, {useState} from 'react';
 
 export default function monthly() {
   const [month, setMonth] = useState(new Date().getMonth() + 1);
-
-    
+  const [year, setYear] = useState(new Date().getFullYear());
+  
   const [days, setDays] = useState([]);
   const [dayOfWeek, setDayOfWeek] = useState(0);
   
